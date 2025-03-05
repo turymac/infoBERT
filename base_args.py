cc62b1001733ab6d4471fd11e465583b9162eabd
turymac <s314872@studenti.polito.it>
2025-03-05 12:55:10 +0100

    fixed comments, fixed evaluation script name, fixed imports

--- base_args.py
+++ base_args.py
@@ -2,15 +2,14 @@ import argparse
 import ast
 
 def add_base_args(parser):
-    parser.add_argument("--model", help="Nome del modello di embedding da usare.",
+    parser.add_argument("--model", help="Model name to use for embedding.",
                         type=str, required=True)
-    parser.add_argument("--clustering-alg", help="Algoritmo di clustering da testare (può essere una lista separata da virgole o 'all').",
-                        type=str, required=True,)
-    parser.add_argument("--metric", help="Metrica per misurare la distanza tra embedding.",
-                        type=str, choices=["cosine", "euclidean", "cityblock", "chebyshev"], default="cosine", )
-    parser.add_argument("--threshold", help="Threshold per discriminare l'informazione.",
+    parser.add_argument("--clustering-alg", help="Clustering algorithm to test (can be a comma-separated list or 'all').",
+                        type=str, required=True)
+    parser.add_argument("--metric", help="Metric to measure the distance between embeddings.",
+                        type=str, choices=["cosine", "euclidean", "cityblock", "chebyshev"], default="cosine")
+    parser.add_argument("--threshold", help="Threshold to distinguish between informative and non-informative content.",
                         type=float, default=0.5)
-    parser.add_argument("--knn", help="Numero di vicini da considerare per lo score finale.",
+    parser.add_argument("--knn", help="Number of nearest neighbors to consider for the final score.",
                         type=int, default=5)
-
     return parser
